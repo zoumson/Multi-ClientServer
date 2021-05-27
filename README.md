62c751ba83ae5675c0569d8334547930594a63e5
zoumson <stargue49@gmail.com>
2021-05-28 01:17:20 +0800

    remove old readme

--- README.md
+++
@@ -1,3 +0,0 @@
-programa con arquitectura cliente servidor en c++ con minimo un servidor y dos clientes.
-
-# MultipleThreadMultipleClientSingleServerSocketCPP
--- compile.sh
+++
@@ -1,3 +0,0 @@
-g++ Server.cpp main_server.cpp MyThread.cpp CustomerSupport.cpp -o server -lpthread
-g++ Client.cpp main_client.cpp Clock.cpp -o client -lpthread
-
